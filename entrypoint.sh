7b26215c969ee28c9a4a7493bfef03a93f8fb666
Abylay Ospan <aospanjokersys.com>
2018-03-31 00:54:49 +0000

    pass TERM signal to hostapd
    Signed-off-by: Abylay Ospan <aospanjokersys.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,4 +57,22 @@ echo "interface=$IFACE" >> /etc/hostapd.conf
 #will reset wlan driver to known state
 /sbin/iw wlan0 set type managed
 
-/usr/sbin/hostapd -d /etc/hostapd.conf
+# SIGTERM-handler
+pid=0
+term_handler() {
+	if [ $pid -ne 0 ]; then
+		kill -SIGTERM "$pid"
+		wait "$pid"
+	fi
+	exit 143; # 128 + 15 -- SIGTERM
+}
+trap 'kill ${!}; term_handler' SIGTERM
+
+/usr/sbin/hostapd -d /etc/hostapd.conf &
+pid="$!"
+
+# wait forever
+while true
+do
+	tail -f /dev/null & wait ${!}
+done
